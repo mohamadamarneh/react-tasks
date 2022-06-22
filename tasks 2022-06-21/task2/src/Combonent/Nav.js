5e0aa6753939e170039de69d6a28030b37a9ceb5
mohamadamarneh <mohamadamarneh7@gmail.com>
2022-06-22 09:28:31 +0300

    tasks2

---
+++ tasks 2022-06-21/task2/src/Combonent/Nav.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Nav extends Component {
+    state = {}
+    render() {
+        return (
+            <React.Fragment>
+                <nav class="navbar navbar-dark bg-dark text-white">
+                    <h1>Hello world</h1>
+                </nav>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Nav;
